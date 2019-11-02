5cc430c093aafd9e5202dc6a61beb6d0b0aab00d
Sander Valstar <sander@email.com>
2019-11-01 17:11:19 -0700

    Removes code coverage tracking from reference solution executables

--- build_scripts/build_reference_solution.sh
+++ build_scripts/build_reference_solution.sh
@@ -2,7 +2,7 @@
 echo "Building reference solution..."
 
 if [ ! -d "build_refsol" ]; then
-    meson build_refsol --default-library=static
+    meson build_refsol -Ddefault_library=static -Db_coverage=false -Doptimization=3 -Dwarning_level=1
 fi
 
 ninja -C build_refsol
