5411c85e0e96eb650bbaf3a05ad318f37f0e4850
rubydoudou <hongkexin97@gmail.com>
2019-11-07 07:23:21 +0000

    except cov

--- test/test_HCTree.cpp
+++ test/test_HCTree.cpp
@@ -10,19 +10,18 @@ using namespace testing;
 
 class SimpleHCTreeFixture : public ::testing::Test {
   protected:
-    HCTree* tree;
+    HCTree tree;
 
   public:
     SimpleHCTreeFixture() {
         // initialization code here
-        tree = new HCTree();
         vector<unsigned int> freqs(256);
         freqs['a'] = 2;
         freqs['b'] = 3;
         freqs['c'] = 4;
         freqs['d'] = 5;
         freqs['e'] = 2;
-        tree->build(freqs);
+        tree.build(freqs);
     }
 };
 
@@ -32,39 +31,37 @@ TEST_F(SimpleHCTreeFixture, TEST_ENCODE) {
     ostringstream os3;
     ostringstream os4;
     ostringstream os5;
-    tree->encode('a', os1);
+    tree.encode('a', os1);
     ASSERT_EQ(os1.str(), "011");
-    tree->encode('b', os2);
+    tree.encode('b', os2);
     ASSERT_EQ(os2.str(), "00");
-    tree->encode('c', os3);
+    tree.encode('c', os3);
     ASSERT_EQ(os3.str(), "10");
-    tree->encode('d', os4);
+    tree.encode('d', os4);
     ASSERT_EQ(os4.str(), "11");
-    tree->encode('e', os5);
+    tree.encode('e', os5);
     ASSERT_EQ(os5.str(), "010");
 }
 
 TEST_F(SimpleHCTreeFixture, TEST_DECODE) {
     istringstream is("11");
-    ASSERT_EQ(tree->decode(is), 'd');
-    delete tree;
+    ASSERT_EQ(tree.decode(is), 'd');
 }
 
 class ComplexHCTreeFixture : public ::testing::Test {
   protected:
-    HCTree* tree2;
+    HCTree tree2;
 
   public:
     ComplexHCTreeFixture() {
         // initialization code here
-        tree2 = new HCTree();
         vector<unsigned int> freqs(256);
         freqs['a'] = 10;
         freqs['b'] = 10;
         freqs['c'] = 10;
         freqs['d'] = 10;
         freqs['e'] = 10;
-        tree2->build(freqs);
+        tree2.build(freqs);
     }
 };
 
@@ -74,39 +71,19 @@ TEST_F(ComplexHCTreeFixture, TEST_ENCODE) {
     ostringstream os3;
     ostringstream os4;
     ostringstream os5;
-    tree2->encode('a', os1);
+    tree2.encode('a', os1);
     ASSERT_EQ(os1.str(), "10");
-    tree2->encode('b', os2);
+    tree2.encode('b', os2);
     ASSERT_EQ(os2.str(), "01");
-    tree2->encode('c', os3);
+    tree2.encode('c', os3);
     ASSERT_EQ(os3.str(), "00");
-    tree2->encode('d', os4);
+    tree2.encode('d', os4);
     ASSERT_EQ(os4.str(), "111");
-    tree2->encode('e', os5);
+    tree2.encode('e', os5);
     ASSERT_EQ(os5.str(), "110");
 }
 
 TEST_F(ComplexHCTreeFixture, TEST_DECODE) {
     istringstream is2("111");
-    ASSERT_EQ(tree2->decode(is2), 'd');
-    delete tree2;
+    ASSERT_EQ(tree2.decode(is2), 'd');
 }
-
-// class OneHCTreeFixture : public ::testing::Test {
-//   protected:
-//     HCTree* tree;
-
-//   public:
-//     OneHCTreeFixture() {
-//         // initialization code here
-//         vector<unsigned int> freqs(256);
-//         freqs['f'] = 2;
-//         tree->build(freqs);
-//     }
-// };
-
-// TEST_F(OneHCTreeFixture, TEST_ENCODE) {
-//     ostringstream os;
-//     tree->encode('f', os);
-//     ASSERT_EQ(os.str(), "0");
-// }
